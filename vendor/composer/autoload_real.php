efde941b535af02dfe6da72f8b45c9d7a873c809
Dral415 <ddaquiga@sfsu.edu>
2017-12-09 14:50:40 -0800

    still trying to connect to cloud sql

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit3a5e8fb51859e28796d0c518c07635ec
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit3a5e8fb51859e28796d0c518c07635ec::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire3a5e8fb51859e28796d0c518c07635ec($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire3a5e8fb51859e28796d0c518c07635ec($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
