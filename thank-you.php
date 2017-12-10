1b65f9d34afd6145ea230dd8e23c5d9336d8568c
Dral415 <ddaquiga@sfsu.edu>
2017-12-09 17:55:27 -0800

    attempting db connection

--- thank-you.php
+++ thank-you.php
@@ -56,7 +56,7 @@ if (empty($_POST["details"]))
 else
 	$details = test_input($_POST["details"]);
 
-$servername = "localhost";
+$servername = "ddaquigan-188101:us-west1:portfolio-instance";
 $username = "root";
 $password = "";
 $dbname = "responses";
