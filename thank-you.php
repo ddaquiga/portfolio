ab019b5aed93078a415966adeb8c7a0f567c9bf3
Dral415 <ddaquiga@sfsu.edu>
2017-12-09 17:21:18 -0800

    attempting db connection

--- thank-you.php
+++ thank-you.php
@@ -56,7 +56,7 @@ if (empty($_POST["details"]))
 else
 	$details = test_input($_POST["details"]);
 
-$servername = 35.203.177.219;
+$servername = "35.203.177.219";
 $username = "root";
 $password = "";
 $dbname = "responses";
