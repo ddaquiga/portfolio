77318874311e050a7ef8401948a790c20e0fd38d
Dral415 <ddaquiga@sfsu.edu>
2017-12-11 16:46:26 -0800

    trying mysqli procedural db connection

--- thank-you.php
+++ thank-you.php
@@ -61,14 +61,13 @@ $username = "root";
 $password = "";
 $dbname = "responses";
 
-$conn= new mysqli($servername, $username, $password, $dbname);
+$conn= mysqli_connect($servername, $username, $password, $dbname);
 
-if ($conn->connect_error){
-	die("Connection failed: " . $conn->connect_error);
+if (!$conn){
+	die("Connection failed: " . mysqli_connect_error());
 }
-else{
-	echo "Connected Successfully";
-}
+echo "Connected Successfully";
+
 
 
 function test_input($data) {
