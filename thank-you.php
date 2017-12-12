71bd7a66b6cb9a0a7ab3bc8a77a4873c71dbdb22
Dral415 <ddaquiga@sfsu.edu>
2017-12-12 12:14:03 -0800

    retrying a version that worked

--- thank-you.php
+++ thank-you.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-
-<html lang="en">
-<head>
+ 
+ <html lang="en">
+ <head>
   <title>Darrel Daquigan</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -9,88 +9,83 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="animations.js"></script>
-</head>
-
-<body id="override-bootstrap">
-<?php 
-  $firstname = $lastname = $company = $email = $phone = $website = $referred = $details = "";
-  
-  if (empty($_POST["firstname"]))
-    $firstname = "";
-  else
-    $firstname = test_input($_POST["firstname"]);
-  
-  if (empty($_POST["lastname"]))
-    $lastname = "";
-  else
-    $lastname = test_input($_POST["lastname"]);
-  
-  if (empty($_POST["company"]))
-    $company = "";
-  else
-    $company = test_input($_POST["company"]);
-  
-  if (empty($_POST["email"]))
-    $email = "";
-  else
-    $email = test_input($_POST["email"]);
-  
-  if (empty($_POST["phone"]))
-    $phone = "";
-  else
-    $phone = test_input($_POST["phone"]);
-  
-  if (empty($_POST["website"]))
-    $website = "";
-  else
-    $website = test_input($_POST["website"]);
-  
-  if (empty($_POST["referred"]))
-    $referred = "";
-  else
-    $referred = test_input($_POST["referred"]);
-  
-  if (empty($_POST["details"]))
-    $details = "";
+ </head>
+ 
+ <body id="override-bootstrap">
+ 
+ <?php 
+ $firstname = $lastname = $company = $email = $phone = $website = $referred = $details = "";
+ 
+ if (empty($_POST["firstname"]))
+  $firstname = "";
+ else
+  $firstname = test_input($_POST["firstname"]);
+ 
+ if (empty($_POST["lastname"]))
+  $lastname = "";
+ else
+  $lastname = test_input($_POST["lastname"]);
+ 
+ if (empty($_POST["company"]))
+  $company = "";
+ else
+  $company = test_input($_POST["company"]);
+ 
+ if (empty($_POST["email"]))
+  $email = "";
+ else
+  $email = test_input($_POST["email"]);
+ 
+ if (empty($_POST["phone"]))
+  $phone = "";
+ else
+  $phone = test_input($_POST["phone"]);
+ 
+ if (empty($_POST["website"]))
+  $website = "";
+ else
+  $website = test_input($_POST["website"]);
+ 
+ if (empty($_POST["referred"]))
+  $referred = "";
+ else
+  $referred = test_input($_POST["referred"]);
+ 
+ if (empty($_POST["details"]))
+  $details = "";
   else
     $details = test_input($_POST["details"]);
   
-  $servername = "35.203.177.219";
+$servername = "35.203.177.219";
   $username = "root";
   $password = "";
   $dbname = "responses";
-  
-  try {
-    $conn= new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch(PDOException $e){
-    echo "Connection failed: " . $e->getMessage();
-  }
-
-  $stmt = $conn->prepare("INSERT INTO employers (firstname, lastname, company, email, phone, website, referred, details) values (:firstname, :lastname, :company, :email, :phone, :website, :referred, :details)");
-  
-  $stmt->bindParam(':firstname',$firstname);
-  $stmt->bindParam(':lastname',$lastname);
-  $stmt->bindParam(':company',$company);
-  $stmt->bindParam(':email',$email);
-  $stmt->bindParam(':phone',$phone);
-  $stmt->bindParam(':website',$website);
-  $stmt->bindParam(':referred',$referred);
-  $stmt->bindParam(':details',$details);
-  $stmt->execute();
-  
-  $conn = null;
-  
-  function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
-?>
-
-<nav class="navbar navbar-default">
+ 
+ try {
+  $conn= new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected Successfully";
+ }
+ catch(PDOException $e){
+  echo "Connection failed: " . $e->getMessage();
+ }
+ 
+ $query = "INSERT INTO employers (firstname, lastname, company, email, phone, website, referred, details) values ('" . $firstname . "','" . $lastname . "','" . $company . "','" . $email . "','" . $phone . "','" . $website . "','" . $referred . "','" . $details . "')";
+ if (!$conn->query($query)){
+  die("Couldn't enter data: " . $conn->error);
+ }
+ 
+ 
+ function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+ }
+ 
+ ?>
+ 
+ <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="index.html"><big><b>Darrel Daquigan</b></big></a>
@@ -101,10 +96,10 @@
       <li><a class="nav-item" href="about-me.html">About Me</a></li>
       <li><a class="nav-item" href="contact.html">Contact</a></li>
       <li class="active"><a class="nav-item" href="hire-me.html">Hire Me</a></li>
-      </ul>
+    </ul>
   </div>
-</nav>
-<div class="container-fluid">
+ </nav>
+ <div class="container-fluid">
   <div class="row">
     <div id="sidebar" class="col-sm-2">
       <ul class="nav nav-pills nav-stacked">
@@ -120,6 +115,6 @@
       <h3>Thank You</h3>
     </div>
   </div>
-</div>
-</body>
-</html>
+ </div>
+ </body>
+ </html>
