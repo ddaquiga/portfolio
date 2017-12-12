f8b4f24c758cb7b0ac572b554945936d974a5f24
Dral415 <ddaquiga@sfsu.edu>
2017-12-11 18:09:46 -0800

    trying again

--- thank-you.php
+++ thank-you.php
@@ -12,6 +12,37 @@
 </head>
 
 <body id="override-bootstrap">
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="index.html"><big><b>Darrel Daquigan</b></big></a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a class="nav-item" href="projects.html">Portfolio</a></li>
+			<li><a class="nav-item" href="resume.html">Resume</a></li>
+			<li><a class="nav-item" href="about-me.html">About Me</a></li>
+			<li><a class="nav-item" href="contact.html">Contact</a></li>
+			<li class="active"><a class="nav-item" href="hire-me.html">Hire Me</a></li>
+		</ul>
+	</div>
+</nav>
+<div class="container-fluid">
+	<div class="row">
+		<div id="sidebar" class="col-sm-2">
+			<ul class="nav nav-pills nav-stacked">
+				<li><a href="index.html"><strong>Home</strong></a></li>
+				<li><a href="projects.html"><strong>Portfolio</strong></a></li>
+				<li><a href="resume.html"><strong>Resume</strong></a></li>
+				<li><a href="about-me.html"><strong>About Me</strong></a></li>
+				<li><a href="contact.html"><strong>Contact</strong></a></li>
+				<li class="active"><a href="hire-me.html"><strong>Hire Me</strong></a></li>
+			</ul>
+		</div>
+		<div id="mainbody" class="col-sm-10">
+			<h3>Thank You</h3>
+		</div>
+	</div>
+</div>
 
 <?php 
 $firstname = $lastname = $company = $email = $phone = $website = $referred = $details = "";
@@ -75,37 +106,5 @@ function test_input($data) {
 }
 
 ?>
-
-<nav class="navbar navbar-default">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<a class="navbar-brand" href="index.html"><big><b>Darrel Daquigan</b></big></a>
-		</div>
-		<ul class="nav navbar-nav">
-			<li><a class="nav-item" href="projects.html">Portfolio</a></li>
-			<li><a class="nav-item" href="resume.html">Resume</a></li>
-			<li><a class="nav-item" href="about-me.html">About Me</a></li>
-			<li><a class="nav-item" href="contact.html">Contact</a></li>
-			<li class="active"><a class="nav-item" href="hire-me.html">Hire Me</a></li>
-		</ul>
-	</div>
-</nav>
-<div class="container-fluid">
-	<div class="row">
-		<div id="sidebar" class="col-sm-2">
-			<ul class="nav nav-pills nav-stacked">
-				<li><a href="index.html"><strong>Home</strong></a></li>
-				<li><a href="projects.html"><strong>Portfolio</strong></a></li>
-				<li><a href="resume.html"><strong>Resume</strong></a></li>
-				<li><a href="about-me.html"><strong>About Me</strong></a></li>
-				<li><a href="contact.html"><strong>Contact</strong></a></li>
-				<li class="active"><a href="hire-me.html"><strong>Hire Me</strong></a></li>
-			</ul>
-		</div>
-		<div id="mainbody" class="col-sm-10">
-			<h3>Thank You</h3>
-		</div>
-	</div>
-</div>
 </body>
 </html>
