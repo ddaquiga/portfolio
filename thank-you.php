d62d7aff37d3c28469fa56c49ca058ad22483b8e
Dral415 <ddaquiga@sfsu.edu>
2017-12-11 16:18:59 -0800

    reverting

--- thank-you.php
+++ thank-you.php
@@ -60,11 +60,11 @@ $servername = "35.203.177.219";
 $username = "root";
 $password = "";
 $dbname = "responses";
-  
-	try {
-    	$conn= new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
+
+try {
+	$conn= new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 $stmt = $conn->prepare("INSERT INTO employers (firstname, lastname, company, email, phone, website, referred, details) values (:firstname, :lastname, :company, :email, :phone, :website, :referred, :details)");
 $stmt->bindParam(':firstname',$firstname);
 $stmt->bindParam(':lastname',$lastname);
@@ -75,50 +75,50 @@ $stmt->bindParam(':website',$website);
 $stmt->bindParam(':referred',$referred);
 $stmt->bindParam(':details',$details);
 $stmt->execute();
-   }
-   catch(PDOException $e){
-   	echo "Connection failed: " . $e->getMessage();
-   }
-   
-   function test_input($data) {
-   	$data = trim($data);
-  	$data = stripslashes($data);
-  	$data = htmlspecialchars($data);
-  	return $data;
-  }
-  
-  ?>
-  
-  <nav class="navbar navbar-default">
-  	<div class="container-fluid">
-  		<div class="navbar-header">
-  			<a class="navbar-brand" href="index.html"><big><b>Darrel Daquigan</b></big></a>
-  		</div>
-  		<ul class="nav navbar-nav">
-  			<li><a class="nav-item" href="projects.html">Portfolio</a></li>
-  			<li><a class="nav-item" href="resume.html">Resume</a></li>
-  			<li><a class="nav-item" href="about-me.html">About Me</a></li>
-  			<li><a class="nav-item" href="contact.html">Contact</a></li>
-  			<li class="active"><a class="nav-item" href="hire-me.html">Hire Me</a></li>
-  		</ul>
-  	</div>
-  </nav>
-  <div class="container-fluid">
-  	<div class="row">
-  		<div id="sidebar" class="col-sm-2">
-  			<ul class="nav nav-pills nav-stacked">
-  				<li><a href="index.html"><strong>Home</strong></a></li>
-  				<li><a href="projects.html"><strong>Portfolio</strong></a></li>
-  				<li><a href="resume.html"><strong>Resume</strong></a></li>
-  				<li><a href="about-me.html"><strong>About Me</strong></a></li>
-  				<li><a href="contact.html"><strong>Contact</strong></a></li>
-  				<li class="active"><a href="hire-me.html"><strong>Hire Me</strong></a></li>
-  			</ul>
-   		</div>
-   		<div id="mainbody" class="col-sm-10">
-   			<h3>Thank You</h3>
-   		</div>
-   	</div>
-   </div>
-  </body>
-  </html>
+}
+catch(PDOException $e){
+	echo "Connection failed: " . $e->getMessage();
+}
+
+function test_input($data) {
+  $data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+?>
+
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="index.html"><big><b>Darrel Daquigan</b></big></a>
+		</div>
+		<ul class="nav navbar-nav">
+			<li><a class="nav-item" href="projects.html">Portfolio</a></li>
+			<li><a class="nav-item" href="resume.html">Resume</a></li>
+			<li><a class="nav-item" href="about-me.html">About Me</a></li>
+			<li><a class="nav-item" href="contact.html">Contact</a></li>
+			<li class="active"><a class="nav-item" href="hire-me.html">Hire Me</a></li>
+		</ul>
+	</div>
+</nav>
+<div class="container-fluid">
+	<div class="row">
+		<div id="sidebar" class="col-sm-2">
+			<ul class="nav nav-pills nav-stacked">
+				<li><a href="index.html"><strong>Home</strong></a></li>
+				<li><a href="projects.html"><strong>Portfolio</strong></a></li>
+				<li><a href="resume.html"><strong>Resume</strong></a></li>
+				<li><a href="about-me.html"><strong>About Me</strong></a></li>
+				<li><a href="contact.html"><strong>Contact</strong></a></li>
+				<li class="active"><a href="hire-me.html"><strong>Hire Me</strong></a></li>
+			</ul>
+		</div>
+		<div id="mainbody" class="col-sm-10">
+			<h3>Thank You</h3>
+		</div>
+	</div>
+</div>
+</body>
+</html>
